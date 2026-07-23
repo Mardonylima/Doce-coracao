eeac20230694dc18940bba5aec84030f5c38f0c0
Mardony Lima <Mardony.Lima.cj12@gmail.com>
2026-07-23 13:44:34 -0300

    a small addition to the layout file: the head tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,11 @@ export default function RootLayout({
       }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Missão JHS</title>
+      </head>
       <body>{children}</body>
     </html>
   );
